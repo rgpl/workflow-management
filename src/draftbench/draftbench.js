5a14a2d1172a08b38e54572615739b0424d8e15f
Gopal <gopalrgpl@gmail.com>
2020-02-07 21:37:22 +0530

    add nav bar

--- src/draftbench/draftbench.js
+++ src/draftbench/draftbench.js
@@ -5,22 +5,61 @@ import {
     EuiHeader,
     EuiHeaderSection,
     EuiHeaderSectionItem,
-    EuiIcon
+    EuiIcon,
+    EuiFlexItem,
+    EuiFlexGroup,
+    EuiSideNav,
+    EuiPageContent,
+    EuiPageContentHeader,
+    EuiTitle,
+    EuiPageContentBody,
+    EuiPageContentHeaderSection
 } from '@elastic/eui';
+import './draftbench.css';
 
 export default class Draftbench extends React.Component {
 
+    
+
     render(){
+        
+
         return(
             <EuiPage>
                 <EuiPageBody>
                     <EuiHeader>
                         <EuiHeaderSection grow={false}>
                             <EuiHeaderSectionItem>
-                            <EuiIcon type="arrowLeft" size="xl" />
+                                <EuiFlexItem className="db-back-icon">
+                                    <EuiIcon type="arrowLeft" size="l" />
+                                </EuiFlexItem>
                             </EuiHeaderSectionItem>
                         </EuiHeaderSection>
                     </EuiHeader>
+                    <EuiFlexGroup>
+                        <EuiFlexItem grow={false}>
+                            <EuiSideNav
+                                
+                                style={{ width: 192 }}
+                            />
+                        </EuiFlexItem>
+                        <EuiFlexItem>
+                            <EuiPageContent>
+
+                                <EuiPageContentHeader>
+                                    <EuiPageContentHeaderSection>
+                                        <EuiTitle>
+                                            <h2>Content title</h2>
+                                        </EuiTitle>
+                                    </EuiPageContentHeaderSection>
+                                    <EuiPageContentHeaderSection>
+                                        Content abilities
+                        </EuiPageContentHeaderSection>
+                                </EuiPageContentHeader>
+                                <EuiPageContentBody>Content body</EuiPageContentBody>
+                            </EuiPageContent>
+                        </EuiFlexItem>
+                    </EuiFlexGroup>
                 </EuiPageBody>
             </EuiPage>
         )
