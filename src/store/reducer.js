9ee088ed7da266fa687cceeefcd267352deb6182
Gopal <gopalrgpl@gmail.com>
2020-02-20 13:51:59 +0530

    remove unwanted block

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,33 +7,6 @@ import {
 
 export default (state = [], action) => {
 
-    let flowRed = {
-        [USER_LOGIN]: ()=>{
-            return [
-                ...state,
-                {
-                  id: action.id,
-                  text: action.text,
-                  completed: false
-                }
-              ]
-        },
-        [CREATE_JOURNEY]: ()=>{
-            return state.map(todo =>
-                (todo.id === action.id)
-                  ? {...todo, completed: !todo.completed}
-                  : todo
-              )
-        },
-        [VIEW_JOURNEY]: ()=>{
-
-        },
-        'default': ()=>{
-
-        }
-    }
-    //flowRed[(action.payload || 'default')]();
-
     switch (action.type) {
       case USER_LOGIN:
         return [
